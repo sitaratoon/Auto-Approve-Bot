29feddf8ad1ad469688dba7904c6726297b78981
sitaratoon <ajay393798@gmail.com>
2025-01-26 22:26:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ADMINS = int(environ.get("ADMINS", ""))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "autoacceptbot")
+DB_NAME = environ.get("DB_NAME", "Ajay")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
