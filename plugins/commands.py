473b8f294f032a10cf4b52fde755f2f87d9da9bd
sitaratoon <ajay393798@gmail.com>
2025-01-27 18:49:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,30 +18,7 @@ ID - <code>{}</code>
 Name - {}</b>
 """
 
-async def get_fsub(bot, message):
-    target_channel_id = AUTH_CHANNEL  # Your channel ID
-    user_id = message.from_user.id
-    try:
-        # Check if user is a member of the required channel
-        await bot.get_chat_member(target_channel_id, user_id)
-    except UserNotParticipant:
-        # Generate the channel invite link
-        channel_link = (await bot.get_chat(target_channel_id)).invite_link
-        join_button = InlineKeyboardButton("🔔 Join Our Channel", url=channel_link)
 
-        # Display a message encouraging the user to join
-        keyboard = [[join_button]]
-        await message.reply(
-            f"<b>👋 Hello {message.from_user.mention()}, Welcome!</b>\n\n"
-            "📢 <b>Exclusive Access Alert!</b> ✨\n\n"
-            "To unlock all the amazing features I offer, please join our updates channel. "
-            "This helps us keep you informed and ensures top-notch service just for you! 😊\n\n"
-            "<i>🚀 Join now and dive into a world of knowledge and creativity!</i>",
-            reply_markup=InlineKeyboardMarkup(keyboard),
-        )
-        return False
-    else:
-        return True
 
 async def broadcast_messages(user_id, message):
     try:
